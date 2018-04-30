5581eddbdb126c2139c13a8ba3f1a29e9fcbddc1
faisal-nstu <faisal.nstu@ymail.com>
2018-04-30 19:15:14 +0600

    tut 27 complete

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,12 +1,29 @@
-﻿var x = 0;
-var s = "";
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
 
-console.log("hello!");
+    console.log("hello!");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    alert("Buying item");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button =
+        $("#buyButton");
+    button.on("click", function () {
+        alert("Buying item");
+    });
+
+    var productInfo = $(".product-props li")
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(500);
+    })
 });
